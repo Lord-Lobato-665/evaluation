852bb95c654100786aa87d8db9566960a76978dc
Gael Lobato <gaelsanchez509@gmail.com>
2025-11-16 10:42:29 -0500

    palindromo completado

---
+++ palindromo/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Ingrese una palabra");
+string palabraOriginal = Console.ReadLine().ToLower();
+
+if(string.IsNullOrEmpty(palabraOriginal))
+{
+    Console.WriteLine("Ingresa una palabra valida");
+    return;
+}
+
+char[] palabraFragmentada = palabraOriginal.ToCharArray();
+int index = palabraFragmentada.Count() - 1;
+int indexInvertido = palabraFragmentada.Count() - palabraFragmentada.Count();
+List<char> listaInvertida = new List<char>();
+
+for(int i = index; i >= 0; i--)
+{
+    listaInvertida.Add(palabraFragmentada[i]);
+}
+
+string palabraInvertida = new string(listaInvertida.ToArray());
+
+if (palabraOriginal.ToLower() == palabraInvertida)
+{
+    Console.WriteLine($"La palabra '{palabraOriginal}' es un palíndromo.");
+}
+else
+{
+    Console.WriteLine($"La palabra '{palabraOriginal}' no es un palíndromo.");
+}
